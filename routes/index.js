315199fd8d1fcf215ac8709100f360811dae5137
namsangwook <flowgrammer@gmail.com>
2016-09-18 17:54:44 +0900

    - working on sign up

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express   = require("express")
     router    = express.Router();
 
+
 router.get("/", function(req, res){
+  console.log('landing');
   res.render("landing");
 });
+
+router.get("/SignUp", function(req, res){
+  console.log('sign up');
+  res.render("signup");
+});
+
 
 module.exports = router;
