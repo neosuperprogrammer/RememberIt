95f06c17aafd35877bbcb3a438905bb7eaf713c4
namsangwook <flowgrammer@gmail.com>
2016-09-18 22:53:23 +0900

    - working on sign up

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express   = require("express")
     router    = express.Router();
 
+var bkfd2Password = require("pbkdf2-password");
+var hasher = bkfd2Password();
+
 
 router.get("/", function(req, res){
   console.log('landing');
@@ -10,6 +13,19 @@ router.get("/", function(req, res){
 router.get("/SignUp", function(req, res){
   console.log('sign up');
   res.render("signup");
+});
+
+router.post("/SignUp", function(req, res){
+  console.log('sign up post');
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log('email : ' + email + ', username : ' + username + ', password : ' + password);
+  hasher({password:password}, function(err, pass, salt, hash){
+
+
+  });
+
 });
 
 
