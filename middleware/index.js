6916b96c2acd269c94afa797d5cab703e040e4e0
namsangwook <flowgrammer@gmail.com>
2016-09-27 02:01:27 +0900

    - working on item delete

--- middleware/index.js
+++ middleware/index.js
@@ -4,14 +4,15 @@ var Items = require('../model/items');
 var contents = {
   checkUserItem: function (req, res, next) {
     if (req.session.user) {
-      var userEmail = req.session.user.email;
-      Items.findById(req.params.id, function (err, foundItem) {
-        if (foundItem.user_email == userEmail) {
-          next();
-        } else {
-          res.send("You don't have permission to do that!");
-        }
-      });
+      next();
+      //var userEmail = req.session.user.email;
+      //Items.findById(req.params.id, function (err, foundItem) {
+      //  if (foundItem.user_email == userEmail) {
+      //    next();
+      //  } else {
+      //    res.send("You don't have permission to do that!");
+      //  }
+      //});
     } else {
       console.log("You need to be signed in to do that!");
       res.redirect("/LogIn");
